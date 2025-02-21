b255f8a820b87d86597c1b3708bb786128ec95a9
Koshambi Bardhan <bardhankoshambi2005@gmail.com>
2025-02-21 12:28:00 +0530

    Create Q5script.js

---
+++ Assignment-10/Q5script.js
@@ -0,0 +1,9 @@
+document.getElementById('lightMode').addEventListener('click', function() {
+    document.body.className = 'light-theme';
+});
+
+document.getElementById('darkMode').addEventListener('click', function() {
+    document.body.className = 'dark-theme';
+});
+
+document.body.className = 'light-theme';
